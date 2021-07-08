d8578388dcc518029196ce33189f81c4c63242a8
Srikar403 <58563346+Srikar403@users.noreply.github.com>
2021-07-08 16:00:20 +0530

    Create issue_list_superindent_search.js

---
+++ sportapp/static/issue_list_superindent_search.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+    $("#location-select").change(function(){
+          // Retrieve the option value and reset the count to zero
+          var locationSelect = $(this).val(), count = 0;
+  
+          $("table tr").each(function(index) {
+          if (index !== 0) {
+  
+              $row = $(this);
+  
+              var id = $row.find("td:eq(8)").text();
+  
+              if (id.indexOf(locationSelect) !== 0) {
+                  $row.hide();
+              }
+              else {
+                  $row.show();
+                  count++;
+  
+              }
+          }
+  
+      });
+          
+  
+      });
+        
+  });
+  
+  
+  function myFunction() {
+      var input, filter, table, tr, td, i, txtValue,locationSelect;
+      input = document.getElementById("myInput");
+      locationSelect= $( "#location-select" ).val();
+  
+      filter = input.value.toUpperCase();
+      table = document.getElementById("myTable");
+      tr = table.getElementsByTagName("tr");
+      for (i = 1; i < tr.length; i++) {
+        var tds = tr[i].getElementsByTagName("td");
+  
+      // hide the row
+      tr[i].style.display = "none";
+  
+      // loop through row cells
+      for (var i2 = 1; i2 <=3; i2++) {
+  
+        // if there's a match
+        if (tds[i2].innerHTML.toUpperCase().indexOf(filter) > -1 ) {
+  
+  
+          // show the row
+          if(tds[8].innerHTML.indexOf(locationSelect)>-1)
+          tr[i].style.display = "";
+  
+          // skip to the next row
+          continue;
+  
+        }
+      }
+    }
+  }
+   
+
