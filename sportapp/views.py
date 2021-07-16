c79ee7c2788dbde8a83539dcd8e9d3c28704c478
kevin32 <kevinpatel301201@gmail.com>
2021-07-17 00:41:07 +0530

    improved css and changed login page

--- sportapp/views.py
+++ sportapp/views.py
@@ -15,16 +15,13 @@ from datetime import datetime
 def isgen(request):
 
     if request.user.email=="n.kailash@iitg.ac.in":
-
-    if request.user.email=="kpatel@iitg.ac.in":
-
         return True
     else :
         return False    
 
 
 def issup(request):
-    if request.user.email=="bkartheek@iitg.ac.in":     # mail id of Superindent
+    if request.user.email=="kpatel@iitg.ac.in":     # mail id of Superindent
         return True
     else :
         return False      
